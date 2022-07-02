3ce7a4612b4783e6ed5c0bf9707844a6071dddcb
Muhib Arshad <muhibarshad123@gmail.com>
2022-07-02 19:11:56 +0500

    Some important points added

---
+++ Some_important_points/Readme.md
@@ -0,0 +1,12 @@
+// E:\Github repository\Zero-to-Advance-OOP-Concepts-CPP\Some_important_points>g++ command_line_arguments.cpp -o program.exe
+
+// E:\Github repository\Zero-to-Advance-OOP-Concepts-CPP\Some_important_points>program.exe "Muhib Arshad" BSSE F2021 BSEF21M540 40
+// The Total number of arguments passed from the command line
+// Including name of the program also =6
+// The arguments that are passed are named as:
+// Argument no 0 program.exe
+// Argument no 1 Muhib Arshad
+// Argument no 2 BSSE
+// Argument no 3 F2021
+// Argument no 4 BSEF21M540
+// Argument no 5 40
---
+++ Some_important_points/command_line_arguments.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<typeinfo>//give the dataType of varible (typeid(varibleName).name()--------except the strings)
+using namespace std;
+int main(int argc,char *argv[])
+{
+    int sum=0;
+    cout<<"The total number of arguments are passed into the main function "<<argc<<endl;
+    //Arguments that are passed are also includes the name of the .exe file
+    cout<<"The arguments that are passed given as:"<<endl;
+    for(int i=0;i<argc;i++)
+    {
+        cout<<"Argument no "<<i<<" "<<argv[i]<<endl;
+        sum+=atoi(argv[i]);
+        /*Atoi:(alphanumberic to integer)
+        1-Converts the only numbers string into the integer
+        2-Coverts the alphabatic string into the 0 integer digit(falsy)
+        */
+    }
+    cout<<"The sum of all the nuumbers strings = "<<sum<<endl;
+    
+    return 0;
+}
