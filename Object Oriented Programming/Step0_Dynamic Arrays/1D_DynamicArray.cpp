80614ab583bf3b56c8c448c090267e9fc1a6703e
Muhib Arshad <muhibarshad123@gmail.com>
2022-08-26 21:32:02 +0500

    1D Dynamic Arrays Added

---
+++ Object Oriented Programming/Step0_Dynamic Arrays/1D_DynamicArray.cpp
@@ -0,0 +1,39 @@
+
+/*Write a program to make a dynamic one dimensional array in C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void inputDynamicArr(int *&arr, int size)
+{
+    cout << "Enter the elements in the array:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+void outputDynamicArr(int *&arr, int size)
+{
+    cout << "The elements in the array:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int size = 5;
+    int *arr = new int[size];
+
+    inputDynamicArr(arr, size);
+    outputDynamicArr(arr, size);
+
+    delete[] arr;
+    arr = nullptr;
+
+    return 0;
+}
