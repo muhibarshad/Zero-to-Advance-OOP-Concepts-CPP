5db4d8e457d1a2089e63714790a2867146ce1e37
Muhib Arshad <muhibarshad123@gmail.com>
2022-08-29 23:21:28 +0500

    Changing

--- Object Oriented Programming/Step1_Classes and objects/Basic Concepts building programs/thisPointer.cpp
+++ Object Oriented Programming/Step1_Classes and objects/Basic Concepts building programs/thisPointer.cpp
@@ -1,4 +1,6 @@
 /*What is a this key Word in C++? Write a C++ program in which the setter function parameter name is same as the class data member name.What is the *this Pointer* in the C++?*/
+/*The this keyword can be used for the two purposes firtsly to use the local variable names as same as the class data memebers name.Secondly it can be used for the function calls chaining in which the local object returns the refernce of the single object and in function chaining it updates the every previous function value also.*/
+
 #include<iostream>
 
 using namespace std;
@@ -33,15 +35,25 @@ public:
     {
         return y;
     }
-    algebra returnObject()
-    {
-        return *this;
-    }
     void getData()
     {
         cout<<"The value of x = "<<x<<endl;
         cout<<"The value of y = "<<y<<endl;
     }
+
+    //member functions
+    algebra &value(int x)
+    {
+        this->x=x;
+        return *this;
+    }
+    algebra &incrementValue(int y)
+    {
+        this->x++;
+        this->y=y;
+        return *this;
+    }
+
 };
 
 int main()
@@ -55,6 +67,8 @@ int main()
     obj1.getData();
     obj2->getData();    
 
+    obj1.value(100).incrementValue(200);
+    obj1.getData();//101,200--->chaining is sequential and the changes made to the object’s data members retains for further chaining calls.
 
 
     return 0;
