66b6d30aa3751b84aad2d90de094c790d0879020
Muhib Arshad <muhibarshad123@gmail.com>
2022-09-13 00:02:14 +0500

    Update namespaces.cpp

--- Some extra concepts/doNotNeverKnow/namespaces.cpp
+++ Some extra concepts/doNotNeverKnow/namespaces.cpp
@@ -8,3 +8,12 @@ int main()
     std::cout<<sub::sum(2,2)<<"\n";
     std::cout<<::sum(2,2)<<"\n";
 }
+
+//Calling the namespace function inside it gives the its local same name function but if we use the scope resolution operaor with it then its fund it in teh global scope abd print it 
+
+// nested namespaces as (namespace goo::foo)
+
+//=================================
+//======Best_Practice___________
+//=================================
+//make the prototypes of all the namepace in the add.h file the make functions in the add.cpp then add the "add.h" header file into the main.
