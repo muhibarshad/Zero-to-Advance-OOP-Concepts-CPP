63c5d05bbfda44e487d4dd669316c27c864752bc
Muhib Arshad <muhibarshad123@gmail.com>
2022-09-12 23:52:30 +0500

    namespaces

---
+++ Some extra concepts/doNotNeverKnow/add.h
@@ -0,0 +1,14 @@
+#include<iostream>
+
+namespace add{
+    int sum(int x,int y)
+    {
+        return x+y;
+    }
+};
+namespace sub{
+    int minus(int x,int y)
+    {
+        return x-y;
+    }
+};
---
+++ Some extra concepts/doNotNeverKnow/namespaces.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"add.h"
+
+int main()
+{
+    std::cout<<"Hello World\n";
+    std::cout<<add::sum(2,2)<<"\n";
+    std::cout<<sub::minus(4,2)<<"\n";
+}
