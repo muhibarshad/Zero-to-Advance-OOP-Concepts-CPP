63ec3ac6a85b8e96c33aa447700821252b7dbf8d
Muhib Arshad <muhibarshad123@gmail.com>
2022-09-13 23:53:18 +0500

    Update staticFunctions.cpp

--- Object Oriented Programming/Step3_DataMembers/staticFunctions.cpp
+++ Object Oriented Programming/Step3_DataMembers/staticFunctions.cpp
@@ -5,21 +5,18 @@ class algebra
 private:
     const int x;
     static int y;
+
 public:
     algebra(int, int);
-    static void incrementY()
-    {
-        y++;
-    }
-    static void printY()
-    {
-        std::cout<<y<<"\n";
-    }
+    static void incrementY();
+    static void printY();
     void getData();
     ~algebra();
 };
 
-algebra::algebra(int x = 0, int y =0) : x(x)
+int algebra::y = 0;
+
+algebra::algebra(int x = 0, int y = 0) : x(x)
 {
     this->y = y;
 }
@@ -28,12 +25,37 @@ void algebra::getData()
     std::cout << "x = " << x << "\n";
     std::cout << "y = " << y << "\n";
 }
+
+//Do not use static while definig
+void algebra::incrementY()
+{
+    algebra::y++;
+}
+void algebra::printY()
+{
+    std::cout << algebra::y << "\n";
+}
+
 algebra::~algebra()
 {
-    std::cout<<"Destructor has been called:\n";
+    std::cout << "Destructor has been called:\n";
 }
 int main()
 {
+    algebra::incrementY();//0++
+    algebra::printY();//1
 
+    algebra obj1;//x=0,y=0
+    obj1.printY();//y=0
+    obj1.incrementY();//y=0++ => y=1
 
+    obj1.getData();//x=0, y=1
+
+    algebra obj2(2,7);//x=2 , static y=7 (ovwrWrited)
+    obj2.getData();
+
+    algebra::printY();//y=7
+    obj1.getData();
+    obj1.incrementY();
+    obj2.getData();//x=2, y=8
 }
