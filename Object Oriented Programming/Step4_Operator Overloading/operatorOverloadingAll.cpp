e99f314392cffcfb0d8a6b9ced15f44222ca8b37
Muhib Arshad <muhibarshad123@gmail.com>
2022-09-22 22:38:04 +0500

    Binary operators

--- Object Oriented Programming/Step4_Operator Overloading/operatorOverloadingAll.cpp
+++ Object Oriented Programming/Step4_Operator Overloading/operatorOverloadingAll.cpp
@@ -55,18 +55,89 @@ public:
     6. Member access ->
     */
 
-   
+    //   1. Arithmetic -, +, *, /, % , +=, -=, *=, /=, %=
+    algebra operator-(const algebra &obj) const
+    {
+        return algebra(x - obj.x, y - obj.y, z - obj.z);
+    }
+    algebra operator+(const algebra &obj) const
+    {
+        return algebra(x + obj.x, y + obj.y, z + obj.z);
+    }
+
+
+    // 2. Relational ==, !=, >=,<=, <, >
+    bool operator==(const algebra &obj) const
+    {
+        return (x == obj.x && y == obj.y && z == obj.z);
+    }
+    bool operator!=(const algebra &obj) const
+    {
+        return !(*this == obj);
+    }
+    bool operator>=(const algebra &obj) const
+    {
+        return (x >= obj.x && y >= obj.y && z >= obj.z);
+    }
+
+
+    // 3. Assignment =
+    void operator=(const algebra &obj)
+    {
+        x = obj.x;
+        y = obj.y;
+        z = obj.z;
+    }
+
+    // 4.Arithematic_Assignment  +=, -=, *=, /=, %=
+
+    algebra operator+=(const algebra &obj)
+    {
+        x += obj.x;
+        y += obj.y;
+        z += obj.z;
+
+        return *this;
+    }
+
+    //-5 Uniary operators
+    void operator ++()
+    {
+        ++x;
+        ++y;
+        ++z;
+    }
+    
 };
 int main()
 {
     algebra a1;
     algebra a2;
-    a1.setData(1,2,3);
-    a2.setData(4,5,6);
+    algebra a3;
+    algebra result;
+    a1.setData(1, 2, 3);
+    a2.setData(4, 5, 6);
+    a3.setData(7, 8, 9);
     a1.putData();
     a2.putData();
+    a3.putData();
 
-    //
+    result = a2 - a1;
+    result.putData();
+    
+    result = a1 + a2+a3;//cash_cading left to right 
+    result.putData();
+
+    a3>=a1 ? cout<<"Object 3 is graeter than Object 2\n" :  cout<<"Object 2 is graeter than Object 3\n";
+ 
+    a2=a3;
+    a2.putData();
+    a3.putData();
+
+    result=a1+=a2+=a3;//has right to left cash cading 
+
+    //cash cading not implemented on the logiacal operators due to no returning of the object
+    result.putData();
 
     return 0;
 }
