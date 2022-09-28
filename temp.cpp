86100c5886e5eb9cdc828810e4225a711f79f7ba
Muhib Arshad <muhibarshad123@gmail.com>
2022-09-28 22:30:40 +0500

    Meme

--- temp.cpp
+++ temp.cpp
@@ -1,113 +1,160 @@
 #include<iostream>
-
 using namespace std;
-
-class MatchScore{
-	int score;
-	int numOfBalls;
-	float sR;
-	bool out;
-	static int totalScore;
-	static int totalBalls;
-	static int totalOut;
-	
-  public:
-   	
-   	MatchScore( int score, int balls, float sR, bool out )
-	{
-   		setScore(score);
-   		setBalls(balls);
-   		setSR(sR);
-   		
-   		this->out = out;
-   		if ( out )
-   			totalOut++;
-	}
-	
-	void setScore( int s )
-	{
-		if ( s >= 0 )
-		{
-			score = s;
-			totalScore += s;
-		}
-		else 
-			score = 0;		
-	}
-	
-	void setBalls( int b )
-	{
-		if ( b >= 1 )
-		{
-			numOfBalls = b;
-			totalBalls += b;
-		}
-		else 
-			numOfBalls = 0;		
-	}
-	
-	void setSR( float s )
-	{
-		if ( s >= 100 )
-			sR = s;
-		else 
-			sR = 0;	
-	}
-	
-	void show() const
-	{
-		cout << score << "\t" <<numOfBalls << "\t\t" << sR << "\t\t";
-		if ( out )
-			cout << "OUT";
-		else 
-			cout << "NOT OUT";
-		
-		cout << '\n';
-	}
-	
-	static int getTotalScore()
-	{
-		return totalScore;
-	}
-	
-	static int getTotalBallsPlayed()
-	{
-		return totalBalls;
-	}
-	
-	static int getTotalPlayersOut()
-	{
-		return totalOut;
-	}
-	
-	static float getAverageStrikeRate()
-	{
-		return (float)totalScore / totalBalls * 100;
-	}
-	
-};
-
-int MatchScore :: totalScore = 0;
-int MatchScore :: totalBalls = 0;
-int MatchScore :: totalOut = 0;
-
 int main()
 {
-	MatchScore players[11] = { MatchScore(12, 8, 150, 1), MatchScore(20, 10, 200.21, 1 ), MatchScore(30, 24, 125.56, 0), MatchScore(60, 34, 176.47, 0), MatchScore(95, 65, 146.11, 1), MatchScore(65, 54, 120.32, 0), MatchScore(90, 75, 120.00, 0), MatchScore(36, 25, 144.92, 1), MatchScore(10, 10, 100.00, 1), MatchScore(23, 15, 154.25, 0), MatchScore(114, 72, 158.33, 1) };
-	int i;
 	
-	cout << "Score\tBalls Played\tStrike Rate\tOut/Not Out\n\n";
-	
-	for ( i = 0; i < 11; i++ )
-	{
-		players[i].show();
-	}
-	
-	cout << "\nTotal Score: " << MatchScore :: getTotalScore();
-	cout << "\nTotal Balls Played: " << MatchScore :: getTotalBallsPlayed();
-	cout << "\nTotal Players Out: " << MatchScore :: getTotalPlayersOut();
-	cout << "\nAverage Strike Rate: " << MatchScore :: getAverageStrikeRate();
-	
-	
+
+//                                    :1ffLLt1;,
+//                                .;LG8888000800Cti
+//                              ,L08800GGGGGGGGG000L
+//                            ,f8@80GGGGGGGGGGGGGCC01
+//                            G@800GGCCCCCCCCCCCCCCCG.
+//                            C80GCCCCCCCGGCCCCCCCLLC;
+//                            i8GCCCCCCCCCCCCCCLCCCLCf
+//                            iCCCLLfffttttfftttfLffLt
+//                           .ttLCffftt1t11LL1111ttfff1i,.
+//                           ,ittCffffLLfffCCffffffffLCt:,,,.
+//                         ,;:,iffffttfft1fLLf1tfffftf1,,..,,,
+//                       .::::;,1Lfftffttt1111tt1ftttt..,:...,:.
+//                      .:.,;:,..;tffLftfffttfff1tfft,.,.;,....:,
+//                      ,. .1;.,  ,1tft11iiiiii11tft, :i:;;..   ;:
+//                     .;,,:i1,... .it1tt111111tttt: :ii;;;;;;::11
+//                 ..:tt1ii1i;;,....:i1ttttttttfti:::iiii1tttt1111:,
+//              .;1tfftt11iii;;::,:,,::;1tttttti;::::;i;iii1ii11ttff1;,
+//            ,1fffttttttt111;:;:,,,,,,,,:;;::,,,,,,:;:iiii111tttttttff;
+//           ;ttttttttt11111ii;;;i;:;::::,,...,,,,:;;iiii11111111ttttt1ti
+//          ,tttt1tt111111111i;;::;;;::,:::;;;;;;;iiiiiiii11111111111tt1t;
+//         ,11tt1111111111111ii;i:,:,:;;::,,:::;;iiiiiiii11111111111111t1t:
+//         11i1ii111111111iiiiii;i;;::;i;;;;:::::;;;;;;iiiiiii11111111iii11.
+//        ;tiii;ii1ii1i111iiiiiiiiii;;;;;;;;;;;;;;;;;;iiiiiiiiiii11iiii;i111
+//       :t1iiii;iiiiiiiiiiiiiiiiiiii;;iiiiiiiii;;i;iiiiiiiiiiiiiiiii;::i1it,
+//       i11i;:,;iiiiiiiiiiiiiiiiiiii;iiiiiiiii;iiiiiiiiiiiiiiiiiiiii:,iiii11
+//      :t1ii:::.:iiiiiiiiiiiiiiiiii;;iiiiiiiii;iiiiiiiiiiiiiiiiiiii;.:;;iii1,
+//     .1iiii;;i,,;;;iiiiiiiiiiiiii;;;;iiiiiiii;;iiiiiiiiiiiiiiiiii;,,;::;iiii.
+//     ;1ii;;;;;:,:i;;;;;;;;;;;iii;;;;;;;iiiiii;;;iiiiiiiiiiii;iii;,,;:::;;;i1:
+//    .1iii;;;;:,,,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiii;,,:::,;;;iiii.
+//    ;iii;;;;;::,,:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;i;;iii;;,.:,,::;;;iiii:
+//   .ii;ii;:::,,..,;:;::::::;:::;;;:::;:;;:ii:;:;;;;;;;i;;i;;;::,.,,:::;;;;;;;i
+
+cout<<"                               .:i1tfffLLLf1i;,,,."<<"\n";
+cout<<"                             .:;;i1tffLfLLCLti:,,,,."<<"\n";
+cout<<"                            .,,,:iLLLLLffffftti;:,,,,."<<"\n";
+cout<<"                            :,,,;11i;;i11ii:,,,,,,,,,,,"<<"\n";
+cout<<"                           ,:..,:;,.,,,:;;::,...,,:,,.,."<<"\n";
+cout<<"                           ;,..,,,:tf1;;;::::::::;;:,..,"<<"\n";
+cout<<"                           ,....,;fLLCCCCLt111tt1;;;,,.,"<<"\n";
+cout<<"                           ....,i1ttffLCCCLfffft1;;::,.,"<<"\n";
+cout<<"                           ....,i11ttfLLLLfffftt1;;:::.,"<<"\n";
+cout<<"                           ::..,;tt1tffLCLLLffffti::::,,"<<"\n";
+cout<<"                           ;i,.:i1ttt1ttffLLfLL1:,,,,,:."<<"\n";
+cout<<"             .1i:      .....11:;11i;;;::,:;1tt;,,::,,::"<<"\n";
+cout<<"             ;0Ct1:..,Lif1,:ifLf11ii11ii;:;ift::;;;;;;:"<<"\n";
+cout<<"              :t1ti::,:,::,,,iLfLftt1t1ttfCLCC;i1ttf1i:"<<"\n";
+cout<<"               ;11ii:;ii1;f0Ci11fiifLttGGCLLLG1;;i11i;,"<<"\n";
+cout<<"               1Lt1111i...;t1::::,,iLftf1itttf1:::::::."<<"\n";
+cout<<"             .iC0GGG00C:...  :1;Gi,,iii;itti;;:::,:;;:"<<"\n";
+cout<<"             tftft1iii;:,:;1tfC;:;:;1t1tttttft1;::;::."<<"\n";
+cout<<"             .1fti;i1ttfLLCG00CffCt,:;;i1ffft1ii;;,,,"<<"\n";
+cout<<"             .LGCffffffffttffftti:,..,,,:i1tfft1;,;;.."<<"\n";
+cout<<"            .;L1tftt1iii111iii:.......,i:,,::::,,:i;..."<<"\n";
+cout<<"           ..Lt:1fLfft11it1,;: ........;t,........ ......."<<"\n";
+cout<<"          .,.G1,i;;;i11ii:.,,,...........,...... ...... ..,.."<<"\n";
+cout<<"         ,,..GG,;;;::;:;i..,:,................,::,.............."<<"\n";
+cout<<"        ,....i@1,;111;it, .,::..............;fLLtf1,..............."<<"\n";
+cout<<"       ,..... t@;:1iiGG, ...:: .............,f00GGC; ................"<<"\n";
+cout<<"      ,....... L8:.18G, ...it1,...............t880G1.................."<<"\n";
+cout<<"     ,..........8L1@C.,;iiitt1iiii:........... t808f .................."<<"\n";
+cout<<"    ............,:;i..,,::,:,,,,................f08t ..................."<<"\n";
+cout<<"    ,...........    ...  ........................fG; ...............  .,"<<"\n";
+cout<<"   ..............................................,i.................;, .,"<<"\n";
+cout<<"   ,................................................................Lt...,"<<"\n";
+cout<<"  ,............................................................... ;i:...,,"<<"\n";
+cout<<".,.................................................................  ....,,"<<"\n";
+cout<<" ,.........................................................................,."<<"\n";
+cout<<" ,..........................................................................,"<<"\n";
+cout<<" ...........................................................................,."<<"\n";
+cout<<"   ..........................................................................,"<<"\n";
+cout<<"       ......,. ..............................................................,"<<"\n";
+cout<<"              ,................................................................,"<<"\n";
+cout<<"               ,...............................................................,."<<"\n";
+cout<<"                , ..............................................................,"<<"\n";
+//                                         ...
+//                                   .,....,,...
+//                                 .,...,..    ..
+//                               .,:;i111ii;::,.  ..
+//                              .,:tLCGCCLLLLt11;,,,
+//                              ,,iLCCLLLfffttftt1;:.
+//                              .,tCLLfLLfttttt111i,.
+//                             :,iCLti:,:;iii;;;iii.
+//                            :fifCLf1;,,;tf, . .,;,.
+//                            1ftLLLLLfffLCCf;ii1ii;.
+//                            ,LCLLfftt111ii;;1ff1i:
+//                             .,fLfft1fLt;::ii;;;1:
+//                               fLfft1i:;:::::,::,
+//                              .fLfftt1;;;:,,,::
+//                              .LLfffff1i;;::::
+//                              ;Lfft1tt1i;;;;,
+//                          ,tttLLft1iii;::::;,
+//                        .t8@0ffffti;;::::::;if;
+//                      :t8@88@Gt111i:::::::;;.L@Ct,.
+//                   .itfG@8888@0f11i;::::::;i,f88@Gt;:
+//                 ,iCGf1fC08@@@80CCLtiiiii1tft0888@81tCti,
+//              .ifG88GGCG0GGLi:,..:LCLLfffLLC000GiitLfLLCGCi
+//            .tG00GCf11i11;:::,:i1fLGCGGCCCG88C1i:.:i;,::;iCf.
+//           .G@880fi;i;1t1ftL00888@888G1CG080GCG00GGCfL0CGti8:
+//           ;@08008GG88888@888888888888C08GG088888@@88888@CCC,
+//           ;GLLffffftC888@888800GCCLff0@CtttfLLCCCGG080@Litt;
+//           :CLffttt1t1fGCCLfLffLLCCG0088080000GGCCLttLtti11ti
+//           :ftt11i1i;;;:;tftLGGG000GGGGGGGGGGGGGGGGCLCC1;iit1.
+
+std::cout<<"                                                    ...,,.."<<"\n";
+std::cout<<"                                               .,::,,,,,,,,."<<"\n";
+std::cout<<"                                              ,::,,:;i;:.,,:,"<<"\n";
+std::cout<<"                                             ,:,:1fLLLLf1:.,:."<<"\n";
+std::cout<<"                                            .:,;fLLLLCLLL1,.,,"<<"\n";
+std::cout<<"                                            .,,111tfft11tt1.,,"<<"\n";
+std::cout<<"                                             ,;i;;itf1ii1tf;i;"<<"\n";
+std::cout<<"                                             .1tfftfLfLLLftti,;."<<"\n";
+std::cout<<"                                             ii1tf11ttffftt;,.:;"<<"\n";
+std::cout<<"                                            :1,;t1i111tttti ...:,"<<"\n";
+std::cout<<"                                           .i:iti11ttttt1i,...,,,"<<"\n";
+std::cout<<"                                        .,:;1fG01i11111i1i1:.,,,::;,"<<"\n";
+std::cout<<"                                    .:i11tttLG8@Li1ii111tiGC:,,:;;111:"<<"\n";
+std::cout<<"                                   .1ttttttfGf1CLitttttt11CL1:,,:;;1tti."<<"\n";
+std::cout<<"                                  .i1111tttLfiitffttttttttfLfi;:::;1ttf;"<<"\n";
+std::cout<<"                                 .1t111111tf1t1tfffLLLLffLLGL1;:::;1tttt,"<<"\n";
+std::cout<<"                                .1t1t11111tttt1tLLLLfLfC0GGGti;:::i1t11tt."<<"\n";
+std::cout<<"                               .it1111i1i1tttt1tGGG0CfC8800Ci;;;;i111111t1."<<"\n";
+std::cout<<"                              .1t11111iii11tttt1CGG8808@8GGL11111tiiii111t1,"<<"\n";
+std::cout<<"                             .it111111i;i11tttt1L0G8@@@@0GGt1ttttt;:iii111ti"<<"\n";
+std::cout<<"                            ,11111111ii;11111tt1fGG0@@@8GGL1ttttt11;;ii1iii1;"<<"\n";
+std::cout<<"                            1t1111i1ii:i11111ttt1C008880GGtttttttt1.:;iii111t:"<<"\n";
+
+//                                          .:i:,,.
+//                                       .:itLLLCCf.
+//                    .:,...             ;GGfi:iL0C1
+//                   :1t:,;11;.          .1Lt1t;:1i1,
+//                  1Li,...:tCf:           ifii;;itt;            .:;;::,
+//                 :0L;:,,::;C8L.           :ttiifLi;..         .;:;LGGGt.
+//                 fGtff;;ttit08i            .ttfCi,,::..       ,ii:iG888t
+//                ;Gf;;;:::::1LGC,            .,t1:;;iii;...    1ii;:1G880,
+//               .C8Gti1t1i;ifLG0i            .1ti;tt11fLi:::  .LC1i;;t880;
+//               i888Gt1iiiit0@88G:           ;fff1tf1;tLt1;;, ,G8LLfi:ifLi.
+//              .C8888Ctii1fL88888C,          ;LCf;:t1;:fCftt; ,G88@G;....,,
+//              :G88@8CLftt1fG88880i         ,tLCGti1L1:i11tL1 .t00C1,.,::..
+//              ;C80GLft1iiiiitCG00i         ;LfC8Ci:ii::1fLCt. ;Lt;i:,,;i,..
+//            .,itft1iii;;::,,:;1tti,.       tCfG0Gt1ttiittfCf,.,:.:1:,:i;:,.
+//          .,;111ttttt1111111111111i,.     .1ftLG008L:::1tfffi,:,,;1::;i:::.
+//         .i;i1111ttttttttttt1111t1:,,.    :fCLCG08G1::1LLCCCf;i::ii;;1i:;:.
+//         ,ti1t1111tttttttt11111tt;,,::    ;ftfGCGCLi:;ff1tLCf11,,;;;1t;;i;.
+//         ,t1ff1111111tttt111111tt;,:;i.   iLCGCGCGLi;tCCfCCL1.,,::;iti:;i;
+//         ,ftff11111tttt111111111ti::;i.  .LGCLtttCti1LLftfff1 .:;;if1:::;:
+//         ,ftfLffftttttttttttttttti::;i. .1GGCfLfLfiiLGft1ftt; ,ii1ti,,,:;,
+//         .11tCCCLLLLfffffffffffLL1::i;  1Gff11ffCt;t0GfLtfft,,i1t1;:...,:.
+//         .11tCLCCCLLLLLLLLLLLLCCC1:;i: ,Cf1LfCL1t1:iCL1t1iti;1tt1iff,....
+//         ,t1tL:tCLLffffLLLLLLLCCCi::i,,fGtfLLG0Cf1itGGLLLtLtf1fCCG0G1.,,,,
+//         :11tt.tCLffffffffffLLLCCi,,;.iLt1C08888GfffLLtff1ftf,;0GGGGCfLCCf.
+// .
+
 	return 0;
 }
