fcdbc9f042d161fc9595e0339782ff5797621b1f
Muhib Arshad <muhibarshad123@gmail.com>
2022-10-01 00:21:38 +0500

    Program

--- Challenge Yourself Here/Challenges/2D_Dynaimic/Answer.cpp
+++ Challenge Yourself Here/Challenges/2D_Dynaimic/Answer.cpp
@@ -1,23 +1,23 @@
 /*
-Write a program that performs following string manipulations: You cannot change 
+Write a program that performs following string manipulations: You cannot change
 prototype of any function.
 1. Write a function that takes two strings i.e., str1 and str2 then appends str1 at the end of str2.
 void StringConcatenate (char *str1, char *str2) ;
 Note: Do not use any extra string inside the function.
-2. Write a function that takes a string and if it finds more than one occurrences of a character in the 
-string, it removes the extra occurrences. 
+2. Write a function that takes a string and if it finds more than one occurrences of a character in the
+string, it removes the extra occurrences.
 Void CompressString(char*);
 For example:
 String: “abadca” String after compression: “abdc”
 String: “aaaaaaabbaad” String after compression: “abd”
 Note: Do not use any extra string inside the function.
-3. Write a function that takes a sentence and returns its inverse. 
+3. Write a function that takes a sentence and returns its inverse.
 Char* ReverseSentence(char*)
 String: “I am Pakistani”
 After calling Reverse Sentence
 String: “Pakistani am I” (Do not change the original string)
-4. Write a function that takes a 2d-array of singular words and convert each singular word of array 
-to a plural word. 
+4. Write a function that takes a 2d-array of singular words and convert each singular word of array
+to a plural word.
 void pluralWords(char **s, int wordCount);
 a. Append ‘es’ in following cases
 1. If a word ends with letter ‘h’ and second last letters are ‘c’ or ‘s’
@@ -39,9 +39,9 @@ Stuff – Staffs
 Lay-off – Lay-offs
 Object Oriented Programming (CS-217) Spring 2021 Section A, B
 National University of Computer and Emerging Sciences, Lahore
-e. If the noun ends with ‘f’ or ‘fe’, the f is changed to ‘ve’ before adding the ‘s’ to form the 
+e. If the noun ends with ‘f’ or ‘fe’, the f is changed to ‘ve’ before adding the ‘s’ to form the
 plural version.
-Wife – Wives, 
+Wife – Wives,
 Wolf – Wolves
 f. If the singular noun ends in ‘on’, then ‘‘on’’ is changed to ‘a’.
 Phenomenon – Phenomena
@@ -72,52 +72,94 @@ Carefully reallocate memory to store plural words.
 
 */
 
-
-#include<iostream>
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
-void increasingSize(char *&str)
+void output(char *str, const int size)
 {
-    
-}
-void StringConcatenate (char *&str1, char *str2);
-int main()
-{
-    char *str1= new char[100];
-    char *str2= new char[100];
-    cin.getline(str1,100);
-    cout<<str1<<"\n";
-    cin.getline(str2,100);
-    cout<<str2<<"\n";
-    StringConcatenate(str1,str2);
-    cout<<str1<<"\n";
-
-    delete[] str1;
-    str1=nullptr;
-    delete[] str2;
-    str2=nullptr;
-
-
-    
-    
-
-    return 0;
-}
-void StringConcatenate (char *&str1, char *str2)
-{
-    char *temp= new char[100];
-    int index=0;
-    for(int i=0;str2[i]!='\0';i++)
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        temp[i]=str2[i];
-        index++;
+        cout << str[i];
     }
-    temp[index++]=' ';
-    for(int i=0;str1[i]!='\0';i++)
+    cout << "\n";
+}
+void input(char *&str, int &size)
+{
+    char ch;
+    do
     {
-        temp[index]=str1[i];
-        index++;
+        ch = getch();
+        if ((ch >= 32 && ch <= 126))
+        {
+            char *temp = new char[size + 1];
+            for (int i = 0; str[i] != '\0'; i++)
+            {
+                temp[i] = str[i];
+            }
+            temp[size - 1] = ch;
+            temp[size] = '\0';
+            delete[] str;
+            str = temp;
+            size++;
+            system("cls");
+            output(str, size);
+        }
+        if (ch == 8)
+        {
+            char *temp = new char[size - 1];
+            for (int i = 0; i < size - 1; i++)
+            {
+                temp[i] = str[i];
+            }
+            temp[size - 2] = '\0';
+            delete[] str;
+            str = temp;
+            size--;
+            system("cls");
+            output(str, size);
+        }
+
+    } while (ch != 13);
+}
+
+int  StringConcatenate(char *&str1, char *str2)
+{
+    int newSize = 1;
+    char *temp = new char[1]{'\0'};
+    for (int i = 0; str1[i] != '\0'; i++)
+    {
+        temp[i] = str1[i];
+        newSize++;
+    }
+    for (int i = 0; str2[i] != '\0'; i++)
+    {
+        temp[newSize] = str2[i];
+        newSize++;
     }
     delete[] str1;
     str1=temp;
+    return newSize;
+
+}
+int main()
+{
+    int size1 = 1;
+    int size2 = 1;
+    char *str1 = new char[size1]{'\0'};
+    char *str2 = new char[size2]{'\0'};
+    cout << "Enter the string 1 :\n";
+    input(str1, size1);
+    cout << "Enter the string 2 :\n";
+    input(str2, size2);
+    output(str1, size1);
+    output(str2, size2);
+    int joinedArray=StringConcatenate(str1,str2);
+    output(str1,joinedArray);
+    delete[] str1;
+    str1 = nullptr;
+    delete[] str2;
+    str2 = nullptr;
+
+    return 0;
 }
